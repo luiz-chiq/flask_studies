e79ec6d5b1f5fe20a55ce80ef59144af02d8ebf1
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-04 04:17:54 -0300

    feat: create createPost route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, redirect, url_for, request
 from tinydb import TinyDB, Query
 from models.user import User
+from models.post import Post
 
 app = Flask(__name__)
 db = TinyDB("db.json")
@@ -26,6 +27,28 @@ def create_user():
     
     users.insert(user.__dict__)
     return jsonify({'message': 'Usuário criado com sucesso!'}), 201
+
+@app.route('/createPost', methods=['POST'])
+def create_post():
+    data = request.get_json()
+    if 'login' not in data or 'content' not in data:
+        return jsonify({'message': 'Dados incompletos'}), 400
+    
+    post = Post(data['login'], data['content'])
+    if not users.contains(Query().login == post.user_login):
+        return jsonify({'message': 'Usuário não existe'}), 409
+    
+    posts.insert(post.__dict__)
+    return jsonify({'message': 'Post feito con sucesso'}), 201
+
+# @app.route('/getPosts', methods=['GET'])
+# def get_posts():
+#     r = ''
+#     for post in posts.all():
+#         print(post)
+#         r += f"<h2>{post['user_login']}</h2>"
+#         r += f"<p>{post['content']}</p>"
+#     return r
 
 if __name__ == '__main__':
     app.run()
