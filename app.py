896e8e11d2195f0ad56d55c7c6580818d8a1366a
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-04 03:42:24 -0300

    refactor(createUser-route): Refactor the createUser route
    I improved the structure and organization of the createUser route to make it more readable and maintainable

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify, redirect, url_for, request
 from tinydb import TinyDB, Query
+from models.user import User
 
 app = Flask(__name__)
-db = TinyDB("todo_db.json")
+db = TinyDB("db.json")
 Todo = Query()  
 
 users = db.table('users')
+posts = db.table('posts')
+likes = db.table('likes')
 
 @app.route('/')
 def hello():
@@ -14,19 +17,15 @@ def hello():
 @app.route('/createUser', methods=['POST'])
 def create_user():
     data = request.get_json()  # Obter os dados do JSON na requisição
-    if 'login' in data and 'name' in data:
-        login = data['login']
-        name = data['name']
-
-        if (users.get(Todo.login == login) != None):
-                return jsonify({'message': 'Usuário já existe'}), 409
-        
-        user = {'login': login, 'name': name}
-        users.insert(user)
-        
-        return jsonify({'message': 'Usuário criado com sucesso!'}), 201
-    else:
+    if 'login' not in data or 'name' not in data:
         return jsonify({'message': 'Dados incompletos'}), 400
+    
+    user = User(data['name'], data['login'])
+    if users.contains(Query().login == user.login):
+        return jsonify({'message': 'Usuário já existe'}), 409
+    
+    users.insert(user.__dict__)
+    return jsonify({'message': 'Usuário criado com sucesso!'}), 201
 
 if __name__ == '__main__':
     app.run()
---
+++ models/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, name, login):
+        self.name = name
+        self.login = login
+    
