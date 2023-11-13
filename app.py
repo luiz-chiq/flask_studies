658df020af478091defdf8d5503a19c45fa2fe6d
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-13 03:57:10 -0300

    chore: remove unused code

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from tinydb import TinyDB
-import uuid
 from flask_jwt_extended import JWTManager
-from utils.modules import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0hl96@pk&h@2=ds$n=#szu+ddkf$21bzrrph40#7zna3c_i^g_'
@@ -12,11 +10,6 @@ jwt = JWTManager(app)
 db = TinyDB("db.json")
 
 from routes import userRoutes, postRoutes, postCommentRoutes, authRoutes
-
-# @app.route('/uuid', methods=['GET'])
-# def generate_uuid():
-#     id = f'<h1>{uuid.uuid4()}</h1>'
-#     return id
 
 if __name__ == '__main__':
     app.run()
